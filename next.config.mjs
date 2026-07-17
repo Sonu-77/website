95f6c5db08d587fe53602d7099f02096c604d47f
Sonu Kachhap <sonu@dreamwaveinnovation.com>
2026-07-17 20:30:43 +0530

    configs added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  basePath: isGithubActions ? "/website" : "",
+  assetPrefix: isGithubActions ? "/website" : "",
   images: {
     unoptimized: true,
   },
